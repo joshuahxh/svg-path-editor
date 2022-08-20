0de4dad37e447eb9ba8cbd4a61ef9d227b181ecf
joshuahxh <joshuahxh@gmail.com>
2022-08-19 20:01:08 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,7 @@ import { UploadImageComponent } from './upload-image/upload-image.component';
 import { ConfigService } from './config.service';
 import { browserComputePathBoundingBox } from './svg-bbox';
 
-export const kDefaultPath = `M 4 8 L 10 1 L 13 0 L 12 3 L 5 9 C 6 10 6 11 7 10 C 7 11 8 12 7 12 A 1.42 1.42 0 0 1 6 13 `
-+ `A 5 5 0 0 0 4 10 Q 3.5 9.9 3.5 10.5 T 2 11.8 T 1.2 11 T 2.5 9.5 T 3 9 A 5 5 90 0 0 0 7 A 1.42 1.42 0 0 1 1 6 `
-+ `C 1 5 2 6 3 6 C 2 7 3 7 4 8 M 10 1 L 10 3 L 12 3 L 10.2 2.8 L 10 1`;
+export const kDefaultPath = `m1 0v-30a2 2 90 0 0 2 -2v-80a3 3 180 0 0 -6 0v80a2 2 90 0 0 2 2v30z`;
 
 @Component({
   selector: 'app-root',
